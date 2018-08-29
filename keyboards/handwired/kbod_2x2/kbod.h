666fb8ebce21b5360662e48cb5d322984a6444d7
YousKab <yousuf.kab@gmail.com>
2018-08-29 15:03:39 -0400

    Added kbod_2x2 test folder
    Test code

---
+++ keyboards/handwired/kbod_2x2/kbod.h
@@ -0,0 +1,15 @@
+#ifndef KBOD_H
+#define KBOD_H
+
+#include "quantum.h"
+
+#define KEYMAP( \
+    k00, k01, k02, \
+    k10, k11, k12 \
+) \
+{ \
+    { k00, k01, k02 }, \
+    { k10, k11, k12 }, \
+}
+
+#endif
