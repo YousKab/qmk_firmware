e3eea94bde10877ee7db8475662d4f290cf2a794
YousKab <yousuf.kab@gmail.com>
2018-08-29 16:52:45 -0400

    RGB changes
    RGB changes

--- keyboards/handwired/kbod_2x2/keymaps/yous/keymap.c
+++ keyboards/handwired/kbod_2x2/keymaps/yous/keymap.c
@@ -1,8 +1,8 @@
-#include "kbod.h"
-//#include "drashna.h"
+#include QMK_KEYBOARD_H
+#include "yous.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  KEYMAP( /* Keypad layer */
+  LAYOUT[0] = ( /* Keypad layer */
        KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6
   )
 };
