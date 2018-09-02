cc48d9b33c833cbd89970689b287f1c844c27fdb
YousKab <yousuf.kab@gmail.com>
2018-09-02 13:33:53 -0400

    Finalized test code for RGB support
    .

--- keyboards/handwired/kbod_2x2/keymaps/yous/keymap.c
+++ keyboards/handwired/kbod_2x2/keymaps/yous/keymap.c
@@ -1,8 +1,16 @@
-#include QMK_KEYBOARD_H
-#include "yous.h"
+// #include QMK_KEYBOARD_H
+// #include "yous.h"
+
+// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//   [0] = LAYOUT( /* Keypad layer */
+//        RGB_TOG,  KC_2, KC_3, KC_4, KC_5, KC_6
+//   )
+// };
+
+#include "kbod_2x2.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Keypad layer */
-       KC_1,  KC_2, KC_3, KC_4, KC_5, KC_6
+  KEYMAP( /* Keypad layer */
+       RGB_TOG,  KC_2, KC_3, KC_4, KC_5, KC_6
   )
 };
